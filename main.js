2e26ea13196ca9cbab1019d6fbd254f41e58efae
Ninedeadeyes <tommykwong_84@hotmail.com>
2022-07-21 17:30:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ window.startGame=function(){
     animate();
 }
 
-let modeSelection=window.confirm("If using touchscreen click 'OK' or mouse click 'Cancel' (Will Adjust Difficulty)");
+let modeSelection=window.confirm("If touchscreen click 'OK' or mouse click 'Cancel'");
 
 if (modeSelection===true){
     type=mobile;
